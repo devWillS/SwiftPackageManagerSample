5e40e363ba8bd74825a31009c37ab6bd02b72993
Seidi Nakamura <seidi.nakamura@st-ventures.jp>
2019-09-23 23:01:39 +0900

    Initial Commit

---
+++ SwiftPackageManagerSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftPackageManagerSample
+//
+//  Created by devWill on 2019/09/23.
+//  Copyright © 2019 devWill. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
